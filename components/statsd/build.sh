125771001dfb78a174b356954d2383eb9b4c80f1
songbinliu <songbin.liu@turbonomic.com>
2017-11-15 18:03:34 -0500

    nits

--- components/statsd/build.sh
+++ components/statsd/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-img=beekman9527/statsd
-
 git clone https://github.com/etsy/statsd.git
 
 cd statsd
 cp ../Dockerfile ./
 cp ../config.js ./
 
+set -x
+img=beekman9527/statsd
 docker build -t $img . 
