72637e5e8737625281a4bf7cb684eb881c49d805
songbinliu <songbin.liu@turbonomic.com>
2017-11-16 11:26:26 -0500

    remove statsd

--- components/statsd_exporter/run.sh
+++ components/statsd_exporter/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -d -p 9125:9125/udp -p 9102:9102 beekman9527/statsd_exporter --debug=true --interval=10
+# if it gets metrics from statsd, then forward port to 9125
+#docker run -d -p 9125:9125/udp -p 9102:9102 beekman9527/statsd_exporter --debug=true --interval=10
+
+# else, if it gets metrics directly from envoy, then forward port to 8125
+docker run -d -p 8125:9125/udp -p 9102:9102 beekman9527/statsd_exporter --debug=true --interval=10
+
