68f6fe16c7527ef9c35aff882cbaf2326ca067ec
songbinliu <songbin.liu@turbonomic.com>
2017-11-16 14:09:08 -0500

    add conf file for mapping

--- components/statsd_exporter/run.sh
+++ components/statsd_exporter/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# if it gets metrics from statsd, then forward port to 9125
-#docker run -d -p 9125:9125/udp -p 9102:9102 beekman9527/statsd_exporter --debug=true --interval=10
+image=beekman9527/statsd_exporter
+conf=`pwd`/conf
+# if it gets metrics from statsd, then it will listen on host port 9125. 
+#    (statsd will listen on port 8125, and send metrics to port 9125.)
+ports="-p 9125:9125/udp -p 9102:9102"
+#docker run -d $ports -v $conf:/etc/statsd_exporter/ $image --debug=true --interval=10
 
-# else, if it gets metrics directly from envoy, then forward port to 8125
-docker run -d -p 8125:9125/udp -p 9102:9102 beekman9527/statsd_exporter --debug=true --interval=10
+# else, if it gets metrics directly from envoy, then it will listen on host port 8125.
+#    (envoy will send metrics to port 8125.)
+ports="-p 8125:9125/udp -p 9102:9102"
+docker run -d  $ports -v $conf:/etc/statsd_exporter/ $image --debug=true --interval=10
 
